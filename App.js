f1d7cb9fe7f93e460bad6d377c616af9e282f76f
samlee00123 <samlee00123@outlook.com>
2020-11-27 20:28:17 +0800

    Simple Exercise

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    press: true,
+    textValue: ''
+  }
+
+  onPress = () => {
+
+    this.setState({
+      textValue: this.state.press ? 'We are Group 7!' : '',
+      press: !this.state.press
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.redbox}>
+        </View>
+        <StatusBar style="auto" />
+        <Button
+          onPress={this.onPress}
+          color="red"
+          title="Button"
+        />
+        <Text>{this.state.textValue}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +40,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  redbox: {
+    width: 200,
+    height: 100,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
 });
